cd6e1b5426d9d5ba89436fc98792feb1b83610f1
GlaucoJRCarvalho <glaucojrcarvalho>
2016-11-26 23:14:38 -0300

    Inserindo Produto com Imagem

--- app/src/main/java/com/example/catenga/projetofinal/CadastrarActivity.java
+++ app/src/main/java/com/example/catenga/projetofinal/CadastrarActivity.java
@@ -1,17 +1,39 @@
 package com.example.catenga.projetofinal;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class CadastrarActivity extends AppCompatActivity {
 
     private ImageButton mSelectImage;
 
+    private EditText mProdutoTitle;
+    private EditText mProdutoDesc;
+
+    private Button mSubmitBtn;
+
+    private Uri mImageUri = null;
+
     private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+
+    private ProgressDialog mProgress;
 
 
     @Override
@@ -19,7 +41,16 @@ public class CadastrarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mProdutoTitle = (EditText) findViewById(R.id.titleField);
+        mProdutoDesc = (EditText) findViewById(R.id.descField);
+
+        mSubmitBtn = (Button) findViewById(R.id.submitBtn);
+
+        mProgress = new ProgressDialog(this);
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +61,39 @@ public class CadastrarActivity extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                startPosting();
+                
+            }
+        });
+    }
+
+    private void startPosting() {
+
+        mProgress.setMessage("Adicionando Produto!");
+        mProgress.show();
+
+        String title_val = mProdutoTitle.getText().toString().trim();
+        String desc_val = mProdutoDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
+
+            StorageReference filepath = mStorage.child("Produto_Images").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+        }
+
     }
 
     @Override
@@ -38,9 +102,9 @@ public class CadastrarActivity extends AppCompatActivity {
 
         if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
 
-            Uri imageUri = data.getData();
+            mImageUri = data.getData();
 
-            mSelectImage.setImageURI(imageUri);
+            mSelectImage.setImageURI(mImageUri);
         }
     }
 }
