b5120678b0b3ec4aa99d74f9a73d9a96cf6e921a
GlaucoJRCarvalho <glaucojrcarvalho>
2016-11-27 16:15:05 -0300

    Concluindo Tarefa

--- app/src/main/java/com/example/catenga/projetofinal/MainActivity.java
+++ app/src/main/java/com/example/catenga/projetofinal/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         mProdutoList.setHasFixedSize(true);
         mProdutoList.setLayoutManager(linearLayoutManager);
 
-        checkUserExist();
+        //checkUserExist();
 
     }
 
